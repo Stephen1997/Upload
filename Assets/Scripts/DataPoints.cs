4082bde7b8c816737c24039432992d29b66897a9
Stephen <stephen77725@outlook.com>
2020-11-18 18:01:54 +0000

    Changed the timer for download

--- Assets/Scripts/DataPoints.cs
+++ Assets/Scripts/DataPoints.cs
@@ -5,24 +5,34 @@ public class DataPoints : MonoBehaviour
 {
     int totalDataPoints = 0;
     bool isTriggered = false;
+    bool isInVicinity = false;
+    float pressedTimer;
     [SerializeField] float downloadTime = 3f;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player" && !isTriggered)
         {
-            StartCoroutine(TimeOfDownload());
+            isInVicinity = true;
         }
     }
-    
-    private IEnumerator TimeOfDownload()
+
+    public void Update()
     {
-        print("first check " + totalDataPoints); 
-        yield return new WaitForSeconds(downloadTime);
-        print("second check " + totalDataPoints);
-        DataPointCollected();
-        isTriggered = true;
-        print("Player made contact " + totalDataPoints);
+        if (Input.GetKey("e") && isInVicinity && !isTriggered)
+        {
+            pressedTimer += Time.deltaTime;
+            if (pressedTimer >= downloadTime)
+            {
+                DataPointCollected();
+                isTriggered = true;
+                print("Player made contact " + totalDataPoints);
+            }
+        }
+        else
+        {
+            pressedTimer = 0f;
+        }
     }
 
     private void DataPointCollected()
