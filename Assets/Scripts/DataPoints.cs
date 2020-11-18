69596a65bd6a1c9b557249c126dcfbd45e217cc2
Stephen <stephen77725@outlook.com>
2020-11-18 15:55:40 +0000

    Added player trigger

---
+++ Assets/Scripts/DataPoints.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DataPoints : MonoBehaviour
+{
+    int totalDataPoints = 0;
+    bool isTriggered = false;
+    [SerializeField] float downloadTime = 3f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !isTriggered)
+        {
+            StartCoroutine(TimeOfDownload());
+        }
+    }
+    
+    private IEnumerator TimeOfDownload()
+    {
+        print("first check " + totalDataPoints); 
+        yield return new WaitForSeconds(downloadTime);
+        print("second check " + totalDataPoints);
+        DataPointCollected();
+        isTriggered = true;
+        print("Player made contact " + totalDataPoints);
+    }
+
+    private void DataPointCollected()
+    {
+        totalDataPoints++;
+    }
+}
