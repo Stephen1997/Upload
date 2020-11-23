ca3663b39783e766bc6a68491b71471485de7349
Stephen <stephen77725@outlook.com>
2020-11-23 08:30:23 +0000

    Fixed data point counting
    You can now only collect each point once, and a global count is increased each time 1 is collected.

--- Assets/Scripts/Core/DataPoints.cs
+++ Assets/Scripts/Core/DataPoints.cs
@@ -1,39 +1,65 @@
-﻿using System.Collections;
+﻿using UPLOAD.Core;
+
+using System.Collections;
 using UnityEngine;
 
-public class DataPoints : MonoBehaviour
+namespace UPLOAD.Core
 {
-    int totalDataPoints = 0;
-    bool isTriggered = false;
-    bool isInVicinity = false;
-    float pressedTimer = 0f;
+    public class DataPoints : MonoBehaviour
+    {       
 
-    [SerializeField] float downloadTime = 3f;
+        bool isTriggered = false;
+        bool isInVicinity = false;
+        float pressedTimer = 0f;
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player" && !isTriggered)
+        [SerializeField] float downloadTime = 3f;
+
+        private TotalDataPointsCollected tdp;
+
+        public void Start()
         {
-            isInVicinity = true;
+            GameObject gob;
+
+            gob = GameObject.Find("DataPoints");
+            tdp = gob.GetComponent<TotalDataPointsCollected>();
         }
-    }
 
-    public void Update()
-    {
-        if (Input.GetKey("e") && isInVicinity && !isTriggered)
+        public void Update()
         {
-            pressedTimer += Time.deltaTime;
-            if (pressedTimer >= downloadTime)
+            if (Input.GetKey("e") && isInVicinity && !isTriggered)
             {
-                totalDataPoints++;
-                isTriggered = true;
-                print("Player made contact " + totalDataPoints);
+                pressedTimer += Time.deltaTime;
+                if (pressedTimer >= downloadTime)
+                {
+                    isTriggered = true;
+                    pressedTimer = 0f;
+                    tdp.IncrementTotal();
+                    print(tdp.GetTotalDataPoints());
+                }
+            }
+            else
+            {
+                pressedTimer = 0f;
             }
         }
-        else
-        {
-            pressedTimer = 0f;
-        }
-    }
 
+        //Detects when player enters vicinity and sets isInVicinity to true
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                isInVicinity = true;
+            }
+        }
+
+        //Detects when player exits vicinity and sets isInVicinity to false
+        void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                isInVicinity = false;
+            }
+        }
+
+    }
 }
---
+++ Assets/Scripts/Core/TotalDataPointsCollected.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UPLOAD.Core
+{
+    public class TotalDataPointsCollected : MonoBehaviour
+    {
+        private int totalDatapoints = 0;
+        int dataPointsInScene = 0;
+
+        public void IncrementTotal()
+        {
+            totalDatapoints++;
+        }
+
+        public int GetTotalDataPoints()
+        {
+            return totalDatapoints;
+        }
+
+        public int GetPointsInScene()
+        {
+            return dataPointsInScene;
+        }
+
+        void Start()
+        {
+            //String reference needs to be changed if the DataPoint tag is ever changed in unity
+            GameObject[] dataPoints = GameObject.FindGameObjectsWithTag("DataPoint");
+
+            dataPointsInScene = dataPoints.Length;
+        }
+    }
+}
